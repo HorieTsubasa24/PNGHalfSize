271edd11d9006c4e5747950b33303af35ada1180
Horie Tsubasa <pass.tsubasa@icloud.com>
2018-03-18 21:05:59 +0900

    Completion

--- PNGResize/Form1.Designer.cs
+++ PNGResize/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ファイル選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "ファイル変換";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 193);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(7, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 168);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "複数選択でまとめて画像のサイズを半分にできます。(.png)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 264);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- PNGResize/Form1.cs
+++ PNGResize/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,125 @@ namespace PNGResize
 {
     public partial class Form1 : Form
     {
+        public string[] pngFileNames;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = "";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "pngファイル(*.png*)|*.png;";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //0番目の「pngファイル」が選択されているようにする
+            ofd.FilterIndex = 0;
+            //タイトルを設定する
+            ofd.Title = "開く画像を選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //複数のファイルを選択できるようにする
+            ofd.Multiselect = true;
+            
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(ofd.FileNames);
+                for(int i = 0; i < ofd.FileNames.Length; i++)
+                    textBox1.Text += ofd.FileNames[i] + ",";
+                pngFileNames = ofd.FileNames;
+            }
+        }
+
+        /// <summary>
+        /// 変換
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] paths = pngFileNames;
+            if (paths == null)
+                return;
+
+            pngFileNames = new string[0];
+            textBox1.Text = "";
+
+            for(int i = 0; i < paths.Length; i++)
+            {
+                BitMapSave(paths[i]);
+                textBox1.Text += "変換" + paths[i] + "\n";
+            }
+        }
+
+        private void BitMapSave(string path)
+        {
+            //存在する？
+            if (path == null || !File.Exists(path))
+            {
+                MessageBox.Show(path + "はありません！");
+                return;
+            };
+
+            //元ファイル
+            string strPic = path;
+
+            string[] strs = strPic.Split('\\');
+
+            //名前のみ 
+            string name = strs[strs.Length - 1];
+
+            //保存先ファイル
+            string strNewPic = "";
+            for (int i = 0; i < strs.Length - 1; i++)
+            {
+                strNewPic += strs[i];
+                strNewPic += '\\';
+            }
+            string folder = strNewPic += "SaveFiles";
+            strNewPic = folder + "\\" + name;
+
+            Bitmap bmpSrc = new Bitmap(@strPic);
+            //元画像の縦横サイズを調べる
+            int width = bmpSrc.Width;
+            int height = bmpSrc.Height;
+
+            if (!File.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            //元画像の縦横サイズを半分にする
+            Bitmap bmpSrcHalf = new Bitmap(bmpSrc, width / 2, height / 2);
+
+
+            //フォーマットをJPGに指定して名前を変えて保存、
+            //デフォルトでSaveを実行するとPNGフォーマットになってしまう。
+            bmpSrcHalf.Save(strNewPic, ImageFormat.Png);
+
+            bmpSrc.Dispose();
+            bmpSrcHalf.Dispose();
         }
     }
 }
